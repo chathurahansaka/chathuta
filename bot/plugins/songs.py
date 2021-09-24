48e0864341142726c375f577d3186734c6a352d9
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-23 18:15:27 -0700

    Update songs.py

--- bot/plugins/songs.py
+++ bot/plugins/songs.py
@@ -27,7 +27,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Searching your song...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -49,21 +49,21 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 Downloading ....")
+    m.edit("`Downloading Song... Please wait ⏱`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Uploaded**'
+        rep = f'**Uploaded**\n@szrosebot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
+        s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur,  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join updates", url=f"https://t.me/sl_bot_zone")]]))
         m.delete()
     except Exception as e:
-        m.edit('❌ Error')
+        m.edit('❌ some error')
         print(e)
 
     try:
