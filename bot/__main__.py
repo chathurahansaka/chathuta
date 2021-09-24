a48a4f8449b8eec72610e72ad8d52213951174e9
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-23 18:36:52 -0700

    start msg photo added

--- bot/__main__.py
+++ bot/__main__.py
@@ -68,7 +68,11 @@ async def start(client, message): #fsub start
 
     else:
         button = None
-    await message.reply(text.format(name, user_id), reply_markup=button)
+                await m.reply_photo(
+                    photo="https://telegra.ph/file/e013f67c7ccddc089171d.png",
+                    reply_markup=button,
+                    caption=f"Hello [{}](tg://user?id={}) 👋\n\nIf you want to know how to use this bot just\ntouch on " `Help` "  Button 👨.format(name, user_id)")
+
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
