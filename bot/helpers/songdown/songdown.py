cc64c8977f36baadd50b639c6e5c3b8c952465a0
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-24 18:52:37 +0530

    Create songdown.py

---
+++ bot/helpers/songdown/songdown.py
@@ -0,0 +1,14 @@
+# To Download Songs.
+from io import BytesIO
+from bot import aiohttpsession as session
+
+
+async def download_song(url):
+    async with session.get(url) as resp:
+        song = await resp.read()
+    song = BytesIO(song)
+    song.name = "a.mp3"
+    return song
+
+# Needed. 
+is_downloading = False
