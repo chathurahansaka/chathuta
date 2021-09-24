274a6f2bb3c1dbbfa0338eb93df69d3c42d6d988
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-09-24 20:11:13 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,7 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot import bot as app
 from bot import LOGGER
+from pyrogram.types import Message
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied #fsub
 from bot.plugins import *
