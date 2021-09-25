f8aebfd380c10792e598e66f5cf9911516a9dfe8
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-09-25 10:32:50 +0530

    Update songs.py

--- bot/plugins/songs.py
+++ bot/plugins/songs.py
@@ -72,10 +72,10 @@ def song(client, message):
     m.edit("`Downloading Song... Please wait ⏱`")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: `YouTube`\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @szrosebot🇱🇰 '
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'**Uploaded**\n@szrosebot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
