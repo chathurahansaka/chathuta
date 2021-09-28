f446bff971c7098471d7eb5151c79cb138b29da2
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-09-28 19:27:36 +0530

    Update Dev.py

--- bot/plugins/Dev.py
+++ bot/plugins/Dev.py
@@ -56,7 +56,7 @@ async def botstats(_, message: Message):
     cpu_usage = psutil.cpu_percent()
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
+    total_users = await db.user_list()
     await message.reply_text(
         text=f"**💫 Bot Stats Of @{BOT_USERNAME} 💫** \n\n**🤖 Bot Version:** `V2.9.1` \n\n**👥 Users:** \n ↳**PM'ed Users:** `{total_users}` \n\n**💾 Disk Usage,** \n ↳**Total Disk Space:** `{total}` \n ↳**Used:** `{used}({disk_usage}%)` \n ↳**Free:** `{free}` \n\n**🎛 Hardware Usage,** \n ↳**CPU Usage:** `{cpu_usage}%` \n ↳**RAM Usage:** `{ram_usage}%`",
         parse_mode="Markdown",
