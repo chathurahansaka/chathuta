8a44aafac83505588bb702477d85362ab491db19
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-11-03 04:21:12 +0530

    Add files via upload

--- bot/__main__.py
+++ bot/__main__.py
@@ -23,12 +23,11 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot import bot as app
 from bot import LOGGER
-from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
-from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied #fsub
+from pyrogram.errors import UserNotParticipant
 from bot.plugins import *
 from pyrogram import idle, filters
-from bot.plugins.Dev import *
-from config import BOT_USERNAME
+from config import BOT_USERNAME, photo
+from helpers.commands import show_status_count
 
 JOIN_ASAP = " **You cant use me untill subscribe our updates channel** ☹️\n\n So Please join our updates channel by the following button and hit on the ` /start ` button again 😊"
 
@@ -38,16 +37,7 @@ FSUBB = InlineKeyboardMarkup(
         ]]      
     )
 
-text = """
-Hello [{}](tg://user?id={}) 👋
-
-I am advance song bot with more features
-
-If you want to know how to use this bot just
-touch on `Help` Button 👨
-"""
-
-@app.on_message(filters.command("start"))
+@app.on_message(filters.command('[start]'))
 async def start(client, message): #fsub start
     try:
         await message._client.get_chat_member(int("-1001325914694"), message.from_user.id)
@@ -56,15 +46,32 @@ async def start(client, message): #fsub start
         text=JOIN_ASAP, disable_web_page_preview=True, reply_markup=FSUBB
     )
         return   #fsub end
-    chat_id = message.chat.id
-    user_id = message.from_user["id"]
-    name = message.from_user["first_name"]
-    if message.chat.type == "private":
-        button = [
+    text = f"""
+Hello {message.from_user.mention} 👋
+
+I am sz song Downloader Bot
+You can download any song useing me
+I can search song useing voice clip
+
+If you want to know how to use me just
+touch on `Help` Button 👨
+
+☬─────────────☬
+🤟 Bot Owner :- [IMkashyapaa ](https://t.me/IMkashyapaa)
+🦅 Powered By :- `【SZ™】`
+☬─────────────☬
+
+⚠️copyright ©️ 2021 [szteambots](https://t.me/szteambots). ** All Rights Reserved** 
+"""   
+    await message.reply_photo(
+                    photo=f"{photo}",
+                    reply_markup=InlineKeyboardMarkup(button),
+                    caption=text)
+button = [
     [
-        InlineKeyboardButton(text="Help button ",  callback_data="help"),
+        InlineKeyboardButton(text="Bot Owner 🤟",  url="https://t.me/IMkashyapaa"),
         InlineKeyboardButton(
-            text="Developers ✨", callback_data="dev"
+            text="Bot's statistics✨", callback_data="stat_callback"
         ),
     ],
     [
@@ -74,16 +81,17 @@ async def start(client, message): #fsub start
         ),
     ],
     [
-        InlineKeyboardButton(text="Add Me To Your Group 🎉", url="http://t.me/{BOT_USERNAME}?startgroup=new"),
+        InlineKeyboardButton(text="Help",  callback_data="help")
+    ],
+    [
+        InlineKeyboardButton(text="Add Me To Your Group 🎉", url=f"http://t.me/{BOT_USERNAME}?startgroup=new"),
     ],
 ]
-        
-    else:
-        button = None
-    await message.reply_photo(
-                    photo="https://telegra.ph/file/1804aa067b165793c6a1a.jpg",
-                    reply_markup=InlineKeyboardMarkup(button),
-                    caption=text.format(name, user_id))
+
+@app.on_callback_query(filters.regex("stat_callback"))
+async def stat_callbacc(_, CallbackQuery):
+    text = await show_status_count
+    await app.answer_callback_query(CallbackQuery.id, text, show_alert=True)            
 
 
 
