3332f578cf5ac56cea7f3967ac19f55e16d406bc
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-11-03 13:47:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -29,29 +29,8 @@ from bot.plugins import *
 from pyrogram import idle, filters
 from bot.plugins.Dev import *
 from config import BOT_USERNAME
+from bot.helpers.fsub import fsub
 
-#Fsub
-def fsub():
-    def fsubfunc(func):
-        @wraps(func)
-        async def checkfsub(_, message, *args, **kwargs):
-            try:
-                await message._client.get_chat_member(int("-1001325914694"), message.from_user.id)
-            except UserNotParticipant:
-               return await message.reply_text(
-        text=" **You cant use me untill subscribe our updates channel** ☹️\n\n So Please join our updates channel by the following button and hit on the ` /start ` button again 😊", disable_web_page_preview=True, 
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join our update Channel 🗣", url="https://t.me/szteambots") ]]))
-            return await func(_, message, *args, **kwargs)
-        return checkfsub
-    return fsubfunc
-
-JOIN_ASAP = " **You cant use me untill subscribe our updates channel** ☹️\n\n So Please join our updates channel by the following button and hit on the ` /start ` button again 😊"
-
-FSUBB = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton(text="Join our update Channel 🗣", url=f"https://t.me/szteambots") 
-        ]]      
-    )
 
 text = """
 Hello [{}](tg://user?id={}) 👋
