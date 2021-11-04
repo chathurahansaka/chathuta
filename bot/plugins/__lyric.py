4c94841ae31623f952eb8614f54668fa0b15196b
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-11-05 05:24:30 +0530

    Update __lyric.py

--- bot/plugins/__lyric.py
+++ bot/plugins/__lyric.py
@@ -28,16 +28,16 @@ from bot import bot as app
 from pyrogram import Client, filters
 
 
-@app.on_message(filters.command(["lyric"]))
+@app.on_message(filters.command(["lyrics"]))
 async def lirik(_, message):
-    rep = await message.reply_text("🔎 **searching lyrics...**")
+    rep = await message.reply_text("🔎 **searching your lyrics...**")
     try:
         if len(message.command) < 2:
             await message.reply_text("**give a lyric name too !**")
             return
         query = message.text.split(None, 1)[1]
         resp = requests.get(f"https://api-tede.herokuapp.com/api/lirik?l={query}").json()
-        result = f"{resp['data']}"
+        result = f"{resp['data']} Downloaded By - @szsongbot"
         await rep.edit(result)
     except Exception as ex:
         print(ex)
