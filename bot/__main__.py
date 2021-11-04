dcf47af6083720acda7c5e88b547c95d2e4c7514
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-04 14:16:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -29,7 +29,7 @@ from pyrogram import idle, filters
 from bot.plugins.Dev import *
 from config import BOT_USERNAME
 from bot.helpers.fsub import fsub
-
+from bot.helpers.database import new_user
 
 text = """
 Hello [{}](tg://user?id={}) 👋
@@ -54,6 +54,7 @@ touch on `Help` Button 👨
 @app.on_message(filters.command("start"))
 @fsub()
 async def start(client, message): #fsub start
+    await new_user(client, message)    
     chat_id = message.chat.id
     user_id = message.from_user["id"]
     name = message.from_user["first_name"]
