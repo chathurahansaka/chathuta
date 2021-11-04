c9aeb4b70cfea82869f32eeb3e9064322e734b45
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-04 16:57:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,10 +23,9 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))	
-	API_HASH = os.environ.get("API_HASH")	
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")	
+	API_ID = int(os.environ.get("API_ID"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL")
