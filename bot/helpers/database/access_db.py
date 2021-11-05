bb227fea508b4dc341a78c3606f77341d021ac3b
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-05 13:12:12 +0530

    Update access_db.py

--- bot/helpers/database/access_db.py
+++ bot/helpers/database/access_db.py
@@ -1,6 +1,7 @@
 import os
 from bot.helpers.database.database import Database
+from configs import Config
 
 BOT_USERNAME = "szsongbot"
 
-db = Database(DATABASE_URL, BOT_USERNAME)
+db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
